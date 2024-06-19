b0c3a572a5b536fc7c97facb10af7deea07d4823
KDvs123 <119438971+KDvs123@users.noreply.github.com>
2024-06-19 21:27:51 +0530

    addded simple tailwind css style for the home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,17 +10,19 @@ const Home = () => {
   }
   return (
     <div>
-      <form action="" onSubmit={submitCode} >
-        <label htmlFor="">Enter The Room Code</label>
+      <form action="" onSubmit={submitCode} className="flex flex-col items-center justify-center" >
+        <label htmlFor="" className="font-bold text-[32px] pt-12">Enter The Room Code</label> <br />
+
         <input
           type="text"
           required
           placeholder="Enter Room Code"
           value={RoomCode}
           onChange={(e) => setRoomCode(e.target.value)}
+          className="bg-blue-500 placeholder:text-white py-2 w-[16rem] rounded-full pl-8 mt-4"
         />
 
-        <button type="submit">Enter Room</button>
+        <button type="submit" className="bg-orange-500 rounded-[0.5rem] mt-4 py-1.5 px-4 font-bold">Enter Room</button>
       </form>
     </div>
   );
