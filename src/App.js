64d3bfd943593c17159765558088967f9dce433c
KDvs123 <119438971+KDvs123@users.noreply.github.com>
2024-06-19 21:10:57 +0530

    created the boiler template for the react app with 2 components home and room

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import {Route, Routes , Navigate} from 'react-router-dom';
+import Home from './components/Home';
+import Room from './components/Room';
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/room/roomID" element={<Room />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
